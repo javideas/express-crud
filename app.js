cbb511a19034d22ce2604b4dc8ec140e915db09e
javideas <infojavideas@gmail.com>
2023-01-23 14:59:52 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const hbs = require("hbs");
+const app = express();
+
+/** Configure engine */
+app.set('view engine', 'hbs');
+app.set("views", `${__dirname}/views`);
+
+//*Configure partials*/
+hbs.registerPartials(__dirname + "/views/partials");
+
+/**  Configure static files */
+app.use(express.static("public"));
+
+const router = require("./config/routes.config");
+app.use(router);
+
+
+app.listen(3000, () => {
+    console.log("Ready!");
+})

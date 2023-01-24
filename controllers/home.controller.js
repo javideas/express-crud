8aa4ac025885e1b3b7ca486cf999b5ca8c2e26d6
javideas <infojavideas@gmail.com>
2023-01-24 16:06:41 +0200

    Started step3 branch

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,3 +1,6 @@
 module.exports.home = (req, res) => {
-    res.render("home");
+    // res.set( "Location", "/users" );
+    // res.status(302).send("redirect!");
+    /** this is the same that: */ 
+    res.redirect("/users");
 }
